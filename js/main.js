8744fcb0a64b3446654554246bee9400a7fea6f5
phuedx <samuel.david.smith@gmail.com>
2012-11-17 21:46:21 +0000

    Search for artist images.
    * Raise artist.search_start when the search is started
    * Raise artist.search_error when the Last.fm response with an error
    * Raise artist.image_found when an image is found

--- js/main.js
+++ js/main.js
@@ -2,7 +2,33 @@ requirejs.config({
     urlArgs: 'ts=' + (new Date).getTime()
 });
 
-requirejs(['app', 'Config', 'lastfm'], function(App, config, LastFM) {
+requirejs(['app', 'Config', 'lastfm', 'jQuery'], function(App, config, LastFM, $) {
     var app = new App(),
-        lastFm = new LastFM(config);
+        lastFm = new LastFM(config),
+        $error = $('#error');
+
+    app.subscribe('artist.search_start', function (name) {
+        $error.hide();
+        lastFm.artist.getInfo({ artist: name }, {
+            success: function (data) {
+                var image = data.artist.image[data.artist.image.length - 1]['#text'];
+                app.publish('artist.image_found', image);
+            },
+            error: function () {
+                app.publish('artist.search_error');
+            }
+        });
+    });
+
+    app.subscribe('artist.search_error', function () {
+        $error.show();
+    });
+
+    $('#lalalalalala').submit(function (e) {
+        e.preventDefault();
+        var name = $('.q', this).val();
+        if ($.trim(name)) {
+            app.publish('artist.search_start', name);
+        }
+    });
 });
