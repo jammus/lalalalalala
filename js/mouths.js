5f49151bfc34fdbf84f06d4ec63b01197b181e19
jammus <jammus@gmail.com>
2012-11-17 21:36:12 +0000

    Hit space to make that fella from Muse talk.

--- js/mouths.js
+++ js/mouths.js
@@ -1,26 +1,48 @@
-$(function() {
-	var canvas = document.getElementById('image');
-	var detector;
-    var image=new Image();
-    var originalImage;
-    image.onload=function(){
-        canvas.width=image.width;
-        canvas.height=image.height;
-        canvas.getContext('2d').drawImage(image,0,0);
-        new HAAR.Detector(haarcascade_mcs_mouth).image(image).complete(function(){
-            for (var i = 0; i < this.objects.length; i++) {
-                var rect=this.objects[i];
-                var ctx=canvas.getContext('2d');
-                var mouth = ctx.getImageData(rect.x, rect.y, rect.width, rect.height);
-                ctx.fillStyle="rgba(0,0,0,1)";
-                ctx.fillRect(rect.x,rect.y,rect.width,rect.height);
-                console.log(mouth);
-                ctx.putImageData(mouth, 0, 0);
-                setTimeout(function() {
-                    ctx.drawImage(image, 0, 0);
-                }, 5000);
+requirejs(['Mousetrap'], function(mousetrap) {
+    var mouths = [ ];
+    $(function() {
+        var canvas = document.getElementById('image');
+        var detector;
+        var image=new Image();
+        var originalImage;
+        var ctx;
+        var resetTimeout;
+        image.onload=function(){
+            ctx = canvas.getContext('2d');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            canvas.getContext('2d').drawImage(image,0,0);
+            new HAAR.Detector(haarcascade_mcs_mouth).image(image).complete(function(){
+                for (var i = 0; i < this.objects.length; i++) {
+                    var rect=this.objects[i];
+                    var mouth = {
+                        image: ctx.getImageData(rect.x, rect.y, rect.width, rect.height),
+                        coords: rect
+                    };
+                    mouths.push(mouth);
+                }
+            }).detect(1, 1.25, 0.1, 1, true);
+        };
+        image.src = 'images/Muse.jpg';
+
+        mousetrap.bind('space', function(evt) {
+            evt.preventDefault();
+            for (var i = 0; i < mouths.length; i++) {
+                var mouth = mouths[i];
+                ctx.fillStyle = "rgba(0, 0, 0, 1)";
+                ctx.fillRect(
+                    mouth.coords.x,
+                    mouth.coords.y,
+                    mouth.coords.width,
+                    mouth.coords.height
+                );
+                ctx.putImageData(mouth.image, mouth.coords.x, mouth.coords.y + (mouth.coords.height / 3));
             }
-        }).detect(1, 1.25, 0.1, 1, true);
-    };
-    image.src='images/LOVE.png';
+            resetTimeout = setTimeout(resetImage, 150);
+        });
+
+        function resetImage() {
+            ctx.drawImage(image, 0, 0);
+        }
+    });
 });
